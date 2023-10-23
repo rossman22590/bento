3d905640d37d6a68e0ece6af14c5ddc56e9a07bc
Ross Cohen <rosscohen1990@gmail.com>
2023-10-23 00:24:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-node'; // or '@sveltejs/adapter-static' or other appropriate adapter
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter({
+      // adapter options
+    }),
+    vite: {
+      // Vite options
+    }
+  }
 };
 
 export default config;
+
