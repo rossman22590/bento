055a4a79d965d3c702c269afd9953aa077545367
Ross Cohen <rosscohen1990@gmail.com>
2023-10-22 22:55:28 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        // Use the Node adapter for deployment
+        adapter: adapter(),
+        // Add other kit configurations if needed
+        // ...
+    }
 };
 
 export default config;
+
