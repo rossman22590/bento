d2f4cfcddd0dfca9da34aa6ad8a0853b19170d8b
Ross Cohen <rosscohen1990@gmail.com>
2023-10-22 23:36:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vitest/config';
 import Unfonts from 'unplugin-fonts/vite';
 
 export default defineConfig({
-  root: './src',
-  build: {
-    rollupOptions: {
-      input: './src/app.html'
-    }
-  },
+  // root: './src',
+  // build: {
+  //   rollupOptions: {
+  //     input: './src/app.html'
+  //   }
+  // },
   plugins: [
     sveltekit(),
     Unfonts({
